fd3337e19c0ace52b9865a04283ba38c417d4445
Deamonkid <Deamon-Kid@noreply.github.com>
2015-04-22 19:19:55 +0000

    Added the first version to the repository

---
+++ morse.py
@@ -0,0 +1,68 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+import argparse
+import os.path
+code = {}
+
+# TODO: Rename this function for better clarity
+def generateMorse(text):
+	result = ""
+	for c in text.upper():
+		if c in code:
+			result += str(code[c])
+	return result.strip()
+
+def parseCommand():
+	parse = argparse.ArgumentParser()
+	parse.add_argument("text")
+	parse.add_argument("-D", "--debug", action="store_true", help="Output Debug Messages")
+	parse.add_argument("-d", "--dit", type=float, default=0.3, help="Defines the duration of a short light")
+	parse.add_argument("-g", "--gpio", type=int, default=4, help="Choose the GPIO to control")
+	parse.add_argument("-c", "--code", default="morseCode.cod", help="File that has the code")
+	return parse.parse_args()
+
+def main():
+	# Parse the Commandline
+	args = parseCommand()
+	DEBUG = args.debug	
+	LED = args.gpio
+	DIT = args.dit
+	DAH = 3*DIT
+
+	#Load the CodeFile
+	if not os.path.isfile(args.code):
+		print "CodeFile does not exist!"
+		return 1
+	for line in file(args.code):
+		tmp = line.split(":")
+		code[tmp[0]] = tmp[1]
+	
+	# Setup the GPIO-Pins		
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(LED,GPIO.OUT)
+	GPIO.output(LED, GPIO.LOW)
+
+	if DEBUG:
+		print vars(args)
+	
+	for word in args.text:
+		for letter in word:
+			sequence = generateMorse(letter)
+			if DEBUG:
+				print sequence
+			for light in sequence:
+				GPIO.output(LED, GPIO.HIGH)
+				if light == ".":
+					time.sleep(DIT)
+				else:
+					time.sleep(DAH)
+				GPIO.output(LED, GPIO.LOW)
+				time.sleep(DIT)
+			time.sleep(2*DIT)
+		time.sleep(4*DIT)
+		if DEBUG:
+			print " "
+	GPIO.cleanup()
+if __name__ == "__main__":
+	sys.exit(main())
