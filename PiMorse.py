7665440c92d198a87779df47cef9828b4a892fd4
Deamon-Kid <Deamon-Kid@users.noreply.github.com>
2015-04-22 23:38:57 +0000

    Added File-Support (-f, --file)

--- PiMorse.py
+++ PiMorse.py
@@ -13,6 +13,7 @@ def parseCommand():
 	parse.add_argument("-d", "--dit", type=float, default=0.3, help="Defines the duration of a short light")
 	parse.add_argument("-g", "--gpio", type=int, default=4, help="Choose the GPIO to control")
 	parse.add_argument("-c", "--code", default="morseCode.cod", help="File that has the code")
+	parse.add_argument("-f", "--file", action="store_true", help="try to read from stdin as filename")
 	return parse.parse_args()
 
 def main():
@@ -22,28 +23,40 @@ def main():
 	LED = args.gpio
 	DIT = args.dit
 	DAH = 3*DIT
+	TEXT = ""
 
+	if DEBUG:
+		print vars(args)
+
+	if args.file:
+		if not os.path.isfile(args.text):
+			print "Error: Could not find file {}".format(args.text)
+			return 1
+		TEXT = "";
+		for line in file(args.text):
+			TEXT += line.strip();
+	else:
+		TEXT = args.text
+	print TEXT
 	# Setup the converter
 	try:
 		morseConverter = Morse.Converter(args.code)
 	except IOError:
+		print "ERROR with {}".format(TEXT)
 		return 1
 
 	# Setup the GPIO-Pins		
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(LED,GPIO.OUT)
 	GPIO.output(LED, GPIO.LOW)
-
-	if DEBUG:
-		print vars(args)
-	
+		
 	# let the LED blink
-	for word in args.text:
+	for word in TEXT:
 		# TODO put this in its function 
 		for letter in word:
 			sequence = morseConverter.toMorse(letter)
 			if DEBUG:
-				print sequence
+				print sequence.strip()
 			for light in sequence:
 				GPIO.output(LED, GPIO.HIGH)
 				if light == ".":
